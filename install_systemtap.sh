6dc6985ad34058a69fa4fd6878516ca2da76fda3
caishui97 <caishui97@163.com>
2016-11-17 10:21:01 +0800

    Create install_systemtap.sh

---
+++ install_systemtap.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+
+release=`uname -a | cut -d' '  -f3`
+
+printf  $release
+
+printf "\n"
+
+function checkok() {
+    if [ $? -eq 0 ]
+    then
+        printf "run complete\n"
+    else
+        printf "something error happen\n "
+        exit
+    fi
+}
+
+# download debug generic for systemtap
+# first install epel,wget,systemtap,kernel-devel
+yum install  -y epel-release
+yum install  -y wget
+yum install  -y systemtap
+yum install  -y kernel-devel-$release
+
+wget http://debuginfo.centos.org/7/x86_64/kernel-debuginfo-common-x86_64-$release.rpm
+checkok
+wget http://debuginfo.centos.org/7/x86_64/kernel-debuginfo-$release.rpm
+checkok
+
+# Installing Required Kernel Information Packages Manually
+rpm -ivh kernel-debuginfo-common-x86_64-$release.rpm
+checkok
+rpm -ivh kernel-debuginfo-$release.rpm
+checkok
+
+printf "Initial Testing \n"
+stap -c df -e 'probe syscall.* { if (target()==pid()) log(name." ".argstr) }'
